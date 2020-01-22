e1ebfe74f94fecf8e22b60eed8ff9634c2208a8d
Nolan Allen <nolan.r.allen@gmail.com>
2020-01-22 14:21:36 -0600

    Initial commit: set up new tables for testing; unable to roll back or change data on dbSchema, unsure how to correct

---
+++ database/migrations/20200122120129_recipes.js
@@ -0,0 +1,88 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('recipes_seeded', seededRecipes => {
+      seededRecipes.increments();
+
+      seededRecipes
+        .string('name', 60)
+        .notNullable();
+      seededRecipes.string('creator');
+      seededRecipes
+        .integer('water_temperature')
+        .notNullable();
+      seededRecipes
+        .string('coarseness')
+        .notNullable();
+      seededRecipes
+        .string('tools_ingredients', 1000)
+        .notNullable();
+      seededRecipes
+        .string('instructions', 1000)
+        .notNullable();
+      seededRecipes
+        .string('yield_time', 60);
+    })
+    .createTable('recipes_users', userRecipes => {
+      userRecipes.increments();
+
+      userRecipes.binary('public_private');
+      userRecipes
+        .string('name', 60)
+        .notNullable();
+      userRecipes.string('creator');
+      userRecipes
+        .integer('water_temperature')
+        .notNullable();
+      userRecipes
+        .string('coarseness')
+        .notNullable();
+      userRecipes
+        .string('tools_ingredients', 1000)
+        .notNullable();
+      userRecipes
+        .string('yield_time', 60);
+    })
+    .createTable('user_instructions', instructions => {
+      instructions.increments();
+
+      instructions
+        .integer('order')
+        .notNullable();
+      instructions
+        .string('content')
+        .notNullable();
+      instructions
+        .integer('recipe_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('recipes_users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+      instructions.integer('yield');
+      instructions.integer('duration');
+    })
+    .createTable('user_tools', tools => {
+      tools.increments();
+
+      tools
+        .string('tool', 60)
+        .notNullable();
+    })
+    .createTable('user_ingredients', ingredients => {
+      ingredients.increments();
+
+      ingredients
+        .string('ingredient', 60)
+        .notNullable();
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('tools')
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('recipes_users')
+    .dropTableIfExists('recipes_seeded');
+};
